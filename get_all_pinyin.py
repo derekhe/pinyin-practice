bf5769bce1abd397b5e063c16cbb51e6ee478639
derekhe <derekhe@april1985.com>
2020-03-17 22:02:41 +0800

    最终版本

--- get_all_pinyin.py
+++ get_all_pinyin.py
@@ -1,11 +1,13 @@
 from pypinyin import pinyin, lazy_pinyin, Style
 
-with open("./resource/words.txt","rt",encoding="utf-8") as f:
+with open("./resource/words.txt", "rt", encoding="utf-8") as f:
     content = f.read()
 
-all_py = set()
+all_py = {}
 for w in content:
-    for py in pinyin(w, style=Style.TONE3, heteronym=True)[0]:
-        all_py.add(py)
+    py = pinyin(w, style=Style.TONE3, heteronym=True)[0][0]
+    if py in all_py:
+        continue
+    all_py[py] = w
 
-print(sorted(all_py))
+print(all_py)
